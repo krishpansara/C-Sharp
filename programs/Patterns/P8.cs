36044f8b675f72a19df3e158e35d7b8b70a6e27c
krishpansara <krishpanasara9265@gmail.com>
2025-07-17 11:32:43 +0530

    patterns

---
+++ programs/Patterns/P8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programs.Patterns
+{
+    internal class P8
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter n : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+   
+            for (int i = 0; i < n+1; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if(i == 0 || i == n || j == 0 || j == 2)
+                    {
+                        Console.Write(n + " ");
+                    }
+                    else
+                    {
+                        Console.Write((i) + " ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ programs/Patterns/P9.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT.Patterns
+{
+    internal class P9
+    {
+        public static void Main(String[] args)
+        {
+            Console.Write("Enter n : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if((i+j) % 2 == 0)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("# ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
