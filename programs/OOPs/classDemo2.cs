d3874c1fb791dd07f991f97c1aabc52b0ef75e37
krishpansara <krishpanasara9265@gmail.com>
2025-08-05 10:57:21 +0530

    demo2

---
+++ programs/OOPs/classDemo2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programs.OOPs
+{
+    class Student
+    {
+        int rollNo;
+        string name;
+
+        /*
+        public Student()
+        {
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            name = Console.ReadLine();
+        }
+        */
+
+        // Default constructor
+        public Student()
+        {
+            Console.WriteLine("Roll No: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter Name: ");
+            name = Console.ReadLine();
+        }
+
+
+        // Parameterized constructor
+        public Student(int rollNo, string name)
+        {
+           this.rollNo = rollNo;
+            this.name = name;
+        }
+
+        public void showData()
+        {
+            Console.WriteLine("Roll No: " + rollNo);
+            Console.WriteLine("Name: " + name);
+        }
+    }
+
+    internal class classDemo2
+    {
+        public static void Main()
+        {
+            Student s1 = new Student(100, "AAA");
+            s1.showData();
+
+            int x = 101;
+            string y = "BBB";
+            Student s2 = new Student(x, y);
+            s2.showData();
+
+
+            Student s3 = new Student();
+            s3.showData();
+
+        }
+    }
+}
