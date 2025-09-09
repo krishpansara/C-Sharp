e3ba1a6a9f3d0228e15d202f6570ac781889c389
krishpansara <krishpanasara9265@gmail.com>
2025-09-09 10:27:32 +0530

    Create inherit_demo.cs

---
+++ programs/OOPs/inheritance/inherit_demo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programs.OOPs.inheritance
+{
+    class A
+    {
+        public void methodA1()
+        {
+            Console.WriteLine("Method-1 From Base A");
+        }
+        void methodA2()
+        {
+            Console.WriteLine("Method-2 from Base A")
+            }
+    }
+
+    class B : A
+    {
+        public void methodB1()
+        {
+            Console.WriteLine("Method-1 From Derived B");
+        }
+    }
+
+    internal class inherit_demo
+    {
+        public static void Main()
+        {
+            B obj = new B();
+            obj.methodA1();
+            obj.methodB1();
+        }   
+    }
+}
