b557c7d521be70374259da3319bfe3726525c335
krishpansara <krishpanasara9265@gmail.com>
2025-10-12 20:51:01 +0530

    Create method_overriding.cs

---
+++ programs/OOPs/method overriding/method_overriding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT.programs.method_overriding
+{
+    class P
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Show method from super class : P");
+        }
+    }
+
+    class Q : P
+    {
+        public override void show()
+        {
+            Console.WriteLine("Show method from sub class : Q");
+        }
+    }
+    internal class method_overriding
+    {
+        public static void main(String[] args)
+        {
+            P objp;
+            Q objq = new Q();
+            objp = objq;
+
+            objp.show();
+            objq.show();
+        }
+    }
+}
